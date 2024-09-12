d78bc9a12bbc9232f1c1126cf86cb7b5aecdc8e3
Md Tasdid Nayem <tasdidnayem@gmail.com>
2024-09-12 14:40:57 +0600

    implement navbar section

--- src/components/Pages/Share/Header/Header.jsx
+++ src/components/Pages/Share/Header/Header.jsx
@@ -1,9 +1,48 @@
+import { Link } from "react-router-dom";
+import logo from "../../../../assets/icons/logo.svg"
 
 
 const Header = () => {
+    const navItems = <>
+        <li><Link to='/'>Home</Link></li>
+    </>
     return (
-        <div>
-            <h1>This is Header Component</h1>
+        <div className="container mx-auto">
+            <div className="navbar bg-base-100 h-28">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            {navItems}
+                        </ul>
+                    </div>
+                    {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
+                    <Link to='/'><img src={logo} alt="" /></Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navItems}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                <button className="btn btn-outline btn-warning">Appointment</button>
+                </div>
+            </div>
         </div>
     );
 };
