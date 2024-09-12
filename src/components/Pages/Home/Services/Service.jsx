c6eabb0de0fee574cb58f957f3e1b38c9600bdd5
Md Tasdid Nayem <tasdidnayem@gmail.com>
2024-09-12 22:04:07 +0600

    compleate services page

---
+++ src/components/Pages/Home/Services/Service.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+const Service = ({ service }) => {
+    const { title, price,img} = service
+    return (
+        <div className="card bg-base-100 shadow-xl p-6">
+            <figure>
+                <img
+                className='rounded-b-2xl'
+                    src={img}
+                    alt="Picture" />
+            </figure>
+            <div>
+                <h2 className="card-title">{title}</h2>
+                <p className='text-orange-600 font-semibold'>Price : ${price}</p>
+            </div>
+        </div>
+    );
+};
+Service.propTypes = {
+    service: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+        img: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,  // Adjust according to your object structure
+    }).isRequired,  // If the entire `service` prop is required
+};
+export default Service;
---
+++ src/components/Pages/Home/Services/Services.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import Service from "./Service";
+
+
+const Services = () => {
+    const [services,setServices]=useState([])
+    useEffect(()=>{
+        fetch('services.json')
+        .then(res=>res.json())
+        .then(data=>setServices(data))
+    },[])
+    console.log(services);
+    return (
+        <div className="container mx-auto py-10">
+            <div className="text-center">
+                <h3 className="text-3xl text-orange-600 font-bold">Service</h3>
+                <h1 className="text-5xl font-semibold">Our Service Area</h1>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which dont look even slightly believable. </p>
+            </div>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8">
+                {
+                    services.map(service=><Service key={service._id} service={service}></Service>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
