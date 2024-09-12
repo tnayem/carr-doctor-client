6f74762bd075461df19f18858a5c8b41d01825cc
Md Tasdid Nayem <tasdidnayem@gmail.com>
2024-09-12 20:17:00 +0600

    banner section compleate

--- src/components/Pages/Home/Banner/Banner.jsx
+++ src/components/Pages/Home/Banner/Banner.jsx
@@ -11,6 +11,17 @@ const Banner = () => {
                     <img
                         src={bannerImg1}
                         className="w-full" />
+                    <div className="absolute h-full bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] flex items-center">
+                        <div className="text-white space-y-7 w-1/3 m-5">
+                            <h1 className="text-6xl font-bold">Affordable Price For Car Servicing</h1>
+                            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                            <div>
+                                <button className="btn btn-primary mr-5">Secondary</button>
+                                <button className="btn btn-outline btn-secondary">Secondary</button>
+                            </div>
+                        </div>
+
+                    </div>
                     <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
                         <a href="#slide4" className="btn btn-circle mr-5">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
@@ -20,6 +31,17 @@ const Banner = () => {
                     <img
                         src={bannerImg2}
                         className="w-full" />
+                    <div className="absolute h-full bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] flex items-center">
+                        <div className="text-white space-y-7 w-1/3 m-5">
+                            <h1 className="text-6xl font-bold">Affordable Price For Car Servicing</h1>
+                            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                            <div>
+                                <button className="btn btn-primary mr-5">Secondary</button>
+                                <button className="btn btn-outline btn-secondary">Secondary</button>
+                            </div>
+                        </div>
+
+                    </div>
                     <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
                         <a href="#slide1" className="btn btn-circle mr-5">❮</a>
                         <a href="#slide3" className="btn btn-circle">❯</a>
@@ -29,6 +51,17 @@ const Banner = () => {
                     <img
                         src={bannerImg3}
                         className="w-full" />
+                    <div className="absolute h-full bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] flex items-center">
+                        <div className="text-white space-y-7 w-1/3 m-5">
+                            <h1 className="text-6xl font-bold">Affordable Price For Car Servicing</h1>
+                            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                            <div>
+                                <button className="btn btn-primary mr-5">Secondary</button>
+                                <button className="btn btn-outline btn-secondary">Secondary</button>
+                            </div>
+                        </div>
+
+                    </div>
                     <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
                         <a href="#slide2" className="btn btn-circle mr-5">❮</a>
                         <a href="#slide4" className="btn btn-circle">❯</a>
@@ -38,6 +71,17 @@ const Banner = () => {
                     <img
                         src={bannerImg4}
                         className="w-full" />
+                    <div className="absolute h-full bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] flex items-center">
+                        <div className="text-white space-y-7 w-1/3 m-5">
+                            <h1 className="text-6xl font-bold">Affordable Price For Car Servicing</h1>
+                            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                            <div>
+                                <button className="btn btn-primary mr-5">Secondary</button>
+                                <button className="btn btn-outline btn-secondary">Secondary</button>
+                            </div>
+                        </div>
+
+                    </div>
                     <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
                         <a href="#slide3" className="btn btn-circle mr-5">❮</a>
                         <a href="#slide1" className="btn btn-circle">❯</a>
